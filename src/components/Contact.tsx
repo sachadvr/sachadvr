764c7ea6ed7411ebd9af0ac291accefdb746294c
Sacha Duvivier <sduvivier@walibuy.com>
2025-01-05 15:12:13 +0100

    📝 Fixing send mailer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Contact.scss'
 import { FaAddressCard } from 'react-icons/fa'
 
+let listener = false;
 const Contact = () => {
+    useEffect(() => {
+        if (listener) return;
+        listener = true;
+        document.getElementById('myForm')?.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const form = document.getElementById('myForm') as HTMLFormElement;
+            const formData = new FormData(form);
+            const payload = {
+                name: formData.get('nom')?.toString() || '',
+                from: formData.get('email')?.toString() || '',
+                phone: formData.get('tel')?.toString() || '',
+                text: formData.get('message')?.toString() || '',
+            };
+            fetch('https://api.sachadvr.fr/mail/sender', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+            })
+            .then((response) => {
+             if (!response.ok) {
+                alert('Une erreur est survenue, veuillez réessayer plus tard');
+                throw new Error('Network response was not ok');
+            }
+             return response.json();
+            })
+            .then(() => {
+                alert('Votre message a bien été envoyé');
+                form.reset();
+            });
+        });
+    }, [])
   return (
     <div className="content contact">
         
         <h2 id="contact grid grid-flow-row items-center "><FaAddressCard /> <span>Me contacter</span></h2>
 
        <p id="desc">Si vous souhaitez plus d&apos;informations, il est notamment possible de me contacter en remplissant le formulaire ci-dessous</p>
-       <form name="my_form" id="myForm" action="mail.php" method="post">
+       <form name="my_form" id="myForm" action="" method="post">
            <p>
                <label htmlFor="nom">Nom de la société ou du particulier</label>
                <input type="text" id="nom" name="nom" required /> <span></span>
